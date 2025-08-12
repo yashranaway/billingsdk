179f916c58ed4c60c410df92fea7a99502b0e17e
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-08-12 22:03:16 +0530

    Add HelloWorld component and registry files

---
+++ src/registry/billingsdk/hello-world.tsx
@@ -0,0 +1,8 @@
+export default function HelloWorld() {
+    return (
+        <div className="flex flex-col">
+            <h1 className="text-2xl font-bold dark:text-white">Hello World</h1>
+            <p className="text-sm text-gray-500 dark:text-gray-400">This is a test</p>
+        </div>
+    )
+}
