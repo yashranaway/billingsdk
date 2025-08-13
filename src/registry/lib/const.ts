4ed5472c41a9d29a0ba5675db8bbe525814290d0
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-08-13 13:17:49 +0530

    Add BillingSDK constants and pricing plans to registry

---
+++ src/registry/lib/const.ts
@@ -0,0 +1,81 @@
+export interface Plan {
+    id: string
+    title: string
+    description: string
+    highlight?: boolean
+    type?: 'monthly' | 'yearly'
+    monthlyPrice: string
+    yearlyPrice: string
+    buttonText: string
+    badge?: string
+    features: {
+        name: string
+        icon: string
+        iconColor?: string
+    }[]
+}
+
+export const plans: Plan[] = [
+    {
+        id: 'free',
+        title: 'Free',
+        description: 'For personal use',
+        monthlyPrice: '0',
+        yearlyPrice: '0',
+        buttonText: 'Get Started',
+        features: [{
+            name: 'Limited access',
+            icon: "check",
+            iconColor: 'text-green-500'
+        }, {
+            name: 'Windows, Linux, Mac support',
+            icon: "check",
+            iconColor: 'text-green-500'
+        }, {
+            name: '24/7 Support',
+            icon: "check",
+            iconColor: 'text-green-500'
+        }]
+    },
+    {
+        id: 'pro',
+        title: 'Pro',
+        description: 'For personal use',
+        monthlyPrice: '10',
+        yearlyPrice: '100',
+        buttonText: 'Purchase',
+        features: [{
+            name: 'Everything in Free',
+            icon: "check",
+            iconColor: 'text-green-500'
+        }, {
+            name: 'Unlimited access',
+            icon: "check",
+            iconColor: 'text-green-500'
+        }, {
+            name: 'Windows, Linux, Mac support',
+            icon: "check",
+            iconColor: 'text-green-500'
+        }]
+    }, {
+        id: 'enterprise',
+        title: 'Enterprise',
+        description: 'For enterprise use',
+        monthlyPrice: '100',
+        yearlyPrice: '1000',
+        buttonText: 'Purchase',
+        features: [{
+            name: 'Everything in Pro',
+            icon: "check",
+            iconColor: 'text-green-500'
+        }, {
+            name: 'Unlimited access',
+            icon: "check",
+            iconColor: 'text-green-500'
+        }, {
+            name: 'Windows, Linux, Mac support',
+            icon: "check",
+            iconColor: 'text-green-500'
+        }]  
+    }
+]
