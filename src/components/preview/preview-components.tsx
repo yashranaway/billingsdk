c8928a6743b8cf69a2b34310ed40b841aa7e6801
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-08-14 13:23:08 +0530

    Update CardHeader component to apply dark theme styles

--- src/components/preview/preview-components.tsx
+++ src/components/preview/preview-components.tsx
@@ -26,7 +26,7 @@ export function PreviewComponents({ className, children }: PreviewComponentsProp
       className={cn("not-prose bg-background", className)} 
       style={themeStyles}
     >
-      <CardHeader className="pb-0">
+      <CardHeader className="pb-0" style={themes[0].cssVars.dark}>
         {/* Tab Buttons */}
         <div className="flex gap-2 justify-between">
           <div className="flex gap-2">
