d80f251c087bc74c47c4522e39b8db45880ffa10
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-08-15 01:22:31 +0530

    update homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,9 @@
-"use client"
-
-import { PricingTableThreeDemo } from '@/components/pricing-table-three-demo';
-import { PricingTableOneDemo } from '@/components/pricing-table-one-demo';
-import { PricingTableTwoDemo } from '@/components/pricing-table-two-demo';
 import Link from 'next/link';
 
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      {/* <HelloWorld /> */}
-      {/* <p className="text-fd-muted-foreground">
+      <p className="text-fd-muted-foreground">
         You can open{' '}
         <Link
           href="/docs"
@@ -18,11 +12,7 @@ export default function HomePage() {
           /docs
         </Link>{' '}
         and see the documentation.
-      </p> */}
-
-      <PricingTableOneDemo />
-      <PricingTableTwoDemo />
-      <PricingTableThreeDemo />
+      </p>
 
     </main>
   );
