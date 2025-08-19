ad4510cfd264e2c31e4d2bd2c5f797e2547878c6
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-08-19 17:06:42 +0530

    add classic themes to pricing table 1 and 2

--- src/components/pricing-table-two-minimal-demo.tsx
+++ src/components/pricing-table-two-minimal-demo.tsx
@@ -3,14 +3,14 @@
 import { plans } from "@/lib/const";
 import { PricingTableTwo } from "@/components/billingsdk/pricing-table-two";
 
-export function PricingTableTwoDemo() {
+export function PricingTableTwoMinimalDemo() {
     return <>
         <PricingTableTwo plans={plans} className="w-full max-w-4xl mx-auto"
             title={`We offer ${plans.length} plans`}
             description="Choose the plan that's right for you"
             onPlanSelect={(planId) => console.log('Selected plan:', planId)}
             size="small" // small, medium, large
-            theme="classic" // minimal or classic
+            theme="minimal" // minimal or classic
         />
     </>
 } 
--- src/registry/billingsdk/demo/pricing-table-two-demo.tsx
+++ src/registry/billingsdk/demo/pricing-table-two-demo.tsx
@@ -5,11 +5,12 @@ import { PricingTableTwo } from "@/components/billingsdk/pricing-table-two";
 
 export function PricingTableTwoDemo() {
     return <>
-    <PricingTableTwo plans={plans} className="w-full max-w-4xl mx-auto"
-        title={`We offer ${plans.length} plans`}
-        description="Choose the plan that's right for you"
-        onPlanSelect={(planId) => console.log('Selected plan:', planId)}
-        variant="small"
+        <PricingTableTwo plans={plans} className="w-full max-w-4xl mx-auto"
+            title={`We offer ${plans.length} plans`}
+            description="Choose the plan that's right for you"
+            onPlanSelect={(planId) => console.log('Selected plan:', planId)}
+            size="small" // small, medium, large
+            theme="classic" // minimal or classic
         />
-        </>
+    </>
 } 
