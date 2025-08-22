3543a682f3eac70eb8e45d6a1f36e24bca438dde
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-08-22 14:28:37 +0530

    home v2

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,19 +1,32 @@
+// import type { ReactNode } from "react";
+// import { HomeLayout } from "fumadocs-ui/layouts/home";
+// import { baseOptions } from "@/app/layout.config";
+
+// export default function Layout({ children }: { children: ReactNode }) {
+//   return (
+//     <HomeLayout {...baseOptions}>
+//       <div
+//         className="max-w-7xl mx-auto border-x border-y border-base-900 px-2 md:px-0 w-full lg:overflow-hidden"
+//         style={{
+//           background:
+//             "repeating-linear-gradient(125deg, transparent, transparent 6px, #e8e8e820 6px, #e8e8e820 7px)",
+//         }}
+//       >
+//         {children}
+//       </div>
+//     </HomeLayout>
+//   );
+// }
+
 import type { ReactNode } from "react";
-import { HomeLayout } from "fumadocs-ui/layouts/home";
-import { baseOptions } from "@/app/layout.config";
+import NavBar from "@/components/landing_v2/NavBar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <HomeLayout {...baseOptions}>
-      <div
-        className="max-w-7xl mx-auto border-x border-y border-base-900 px-2 md:px-0 w-full lg:overflow-hidden"
-        style={{
-          background:
-            "repeating-linear-gradient(125deg, transparent, transparent 6px, #e8e8e820 6px, #e8e8e820 7px)",
-        }}
-      >
-        {children}
-      </div>
-    </HomeLayout>
+    <div className="w-full max-w-[1920px] mx-auto min-h-screen bg-background">
+      <NavBar />
+      {/* Main Content */}
+      {children}
+    </div>
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,21 +1,43 @@
-"use client";
+// "use client";
 
-import Hero from "@/components/landing/Hero";
-import Features from "@/components/landing/Features";
-import { CodeSection } from "@/components/landing/code-section";
-import { Footer } from "@/components/landing/footer";
-import { ComponentsSection } from "@/components/landing/components";
+// import Hero from "@/components/landing/Hero";
+// import Features from "@/components/landing/Features";
+// import { CodeSection } from "@/components/landing/code-section";
+// import { Footer } from "@/components/landing/footer";
+// import { ComponentsSection } from "@/components/landing/components";
+// import { Footer2 } from "@/components/landing/footer2";
+
+// export default function HomePage() {
+//   return (
+//     <main className="max-w-6xl bg-background 2xl:max-w-[1400px] mx-auto border-x flex flex-col items-end border-t border-border">
+//       <Hero />
+//       <Features />
+//       <ComponentsSection />
+//       <CodeSection />
+//       <Footer />
+//       <Footer2 />
+//     </main>
+//   );
+// }
+
+
+import React from "react";
 import { Footer2 } from "@/components/landing/footer2";
+import Hero from "@/components/landing_v2/Hero";
+import { ComponentsSection } from "@/components/landing/components";
+import Features from "@/components/landing/Features";
+import { Footer } from "@/components/landing/footer";
 
-export default function HomePage() {
+const Page = () => {
   return (
-    <main className="max-w-6xl bg-background 2xl:max-w-[1400px] mx-auto border-x flex flex-col items-end border-t border-border">
+    <main className="w-full relative overflow-hidden p-4">
       <Hero />
       <Features />
       <ComponentsSection />
-      <CodeSection />
       <Footer />
       <Footer2 />
     </main>
   );
-}
+};
+
+export default Page;
