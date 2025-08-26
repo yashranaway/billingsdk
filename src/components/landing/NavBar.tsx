0f2772ee1d31e680f2ffe942551f2de77342bdbd
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2025-08-27 02:26:44 +0530

    navbar optimise

--- src/components/landing/NavBar.tsx
+++ src/components/landing/NavBar.tsx
@@ -1,71 +1,70 @@
 "use client";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import { Github } from "lucide-react";
 import { cn } from "@/lib/utils";
-
+import { useMotionValueEvent, useScroll } from "motion/react";
+import { useState } from "react";
 
 export const Logo = () => {
   return (
     <div className="flex items-center justify-center gap-2 mr-8">
-    <Image
-      src="/logo/logo-dodo.svg"
-      alt="Billing SDK"
-      width={28}
-      height={28}
-    />
-    <span className="text-3xl font-display">/</span>
+      <Image
+        src="/logo/logo-dodo.svg"
+        alt="Billing SDK"
+        width={28}
+        height={28}
+      />
+      <span className="text-3xl font-display">/</span>
       <Image src="/logo/Logo.svg" alt="Billing SDK" width={120} height={120} />
-
-  </div>
+    </div>
   );
 };
 
 const NavBar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 20);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
+  const { scrollYProgress, scrollY } = useScroll();
+  useMotionValueEvent(scrollY, "change", (current) =>
+    setIsScrolled(current >= 20)
+  );
   return (
     <nav
       className={cn(
         `fixed top-0 left-0 right-0 z-55 flex justify-center ${
-          isScrolled ? "py-0" : "p-5"} transition-all duration-300`
+          isScrolled ? "py-0" : "p-5"
+        } transition-all duration-300`
       )}
     >
       <div className="max-w-[1920px] mx-auto w-full">
         <div
           className={cn(
             `flex items-center w-full justify-between px-6 py-3 transition-all duration-300 ${
-              isScrolled && "bg-white/10 backdrop-blur-md border border-white/20 rounded-b-lg"
+              isScrolled &&
+              "bg-white/10 backdrop-blur-md border border-white/20 rounded-b-lg"
             }`
           )}
         >
-        <Logo />
+          <Logo />
 
-        {/* Join Beta Button */}
-        <div className="flex items-center gap-2">
-          <Button variant="ghost" size="sm" asChild>
-            <Link
-              href="https://github.com/dodopayments/billingsdk"
-              target="_blank"
-              rel="noopener noreferrer"
+          {/* Join Beta Button */}
+          <div className="flex items-center gap-2">
+            <Button variant="ghost" size="sm" asChild>
+              <Link
+                href="https://github.com/dodopayments/billingsdk"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Github className="h-4 w-4" />
+              </Link>
+            </Button>
+            <Button
+              size="sm"
+              className="bg-primary text-primary-foreground ring-primary before:from-primary-foreground/20 after:from-primary-foreground/10 relative isolate inline-flex items-center justify-center overflow-hidden rounded-md px-3 text-left text-sm font-medium ring-1 before:pointer-events-none before:absolute before:inset-0 before:-z-10 before:rounded-md before:bg-gradient-to-b before:opacity-80 before:transition-opacity before:duration-300 before:ease-[cubic-bezier(0.4,0.36,0,1)] after:pointer-events-none after:absolute after:inset-0 after:-z-10 after:rounded-md after:bg-gradient-to-b after:to-transparent after:mix-blend-overlay hover:cursor-pointer"
+              asChild
             >
-              <Github className="h-4 w-4" />
-            </Link>
-          </Button>
-          <Button size="sm" className='bg-primary text-primary-foreground ring-primary before:from-primary-foreground/20 after:from-primary-foreground/10 relative isolate inline-flex items-center justify-center overflow-hidden rounded-md px-3 text-left text-sm font-medium ring-1 before:pointer-events-none before:absolute before:inset-0 before:-z-10 before:rounded-md before:bg-gradient-to-b before:opacity-80 before:transition-opacity before:duration-300 before:ease-[cubic-bezier(0.4,0.36,0,1)] after:pointer-events-none after:absolute after:inset-0 after:-z-10 after:rounded-md after:bg-gradient-to-b after:to-transparent after:mix-blend-overlay hover:cursor-pointer' asChild>
-            <Link href="/docs">Get Started</Link>
-          </Button>
+              <Link href="/docs">Get Started</Link>
+            </Button>
           </div>
         </div>
       </div>
