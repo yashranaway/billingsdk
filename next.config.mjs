2f34179d2e61c3e01c93cf2e4e4b2e508bfa9c83
Kartik Labhshetwar <kartik.labhshetwar@gmail.com>
2025-08-28 18:21:20 +0530

    perf: optimize images and fix linting warnings

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,22 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'framerusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
