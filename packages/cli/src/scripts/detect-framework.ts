f3195cf7f84bc0071e6c6ca96a40c927a75a44cb
diwanshu <diwanshu63019@gmail.com>
2025-09-08 17:01:46 +0530

    fix: hasFile() to check located project root, checks for .next, next.config.cjs

--- packages/cli/src/scripts/detect-framework.ts
+++ packages/cli/src/scripts/detect-framework.ts
@@ -1,26 +1,37 @@
 import { existsSync, readFileSync } from "node:fs";
-import { findUpSync } from "find-up"; // find a directory by walking up parent directories
+import path from "node:path";
+import { findUpSync } from "find-up"; // find a file path by walking up parent directories
 
 export const detectFramework = (): "nextjs" | "express" | "react" | null => {
     try {
 
-        const pkgPath = findUpSync("package.json")
+        const pkgPath = findUpSync("package.json");
         if (!pkgPath) return null;
-        const pkg = JSON.parse(readFileSync(pkgPath, "utf-8"))
+        const rootDir = path.dirname(pkgPath);
+        const pkg = JSON.parse(readFileSync(pkgPath, "utf-8"));
 
         function hasFile(file: string) {
-            return existsSync(file)
+            return existsSync(path.join(rootDir, file));
         }
-        const deps = {
-            ...pkg.dependencies,
-            ...pkg.devDependencies,
-            ...pkg.peerDependencies
-        }
-        
+        const {
+            dependencies = {},
+            devDependencies = {},
+            peerDependencies = {},
+        } = (pkg ?? {});
+        const deps = { ...dependencies, ...devDependencies, ...peerDependencies };
+
         //  nextjs detection
-        if (deps.next || hasFile("next.config.js") || hasFile("next.config.mjs") || hasFile("next.config.ts")) {
+        if (
+            deps.next ||
+            hasFile("next.config.js") ||
+            hasFile("next.config.mjs") ||
+            hasFile("next.config.cjs") ||
+            hasFile("next.config.ts") ||
+            existsSync(path.join(rootDir, ".next"))
+        ) {
             return "nextjs";
         }
+        
         //  express detection
         if (deps.express) {
             return "express";
