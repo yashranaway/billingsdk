ab1c41ed0ea84696c3c4afcdcc16469cd23a2df2
rohanshrma222 <hackerviruse04@gmail.com>
2025-09-17 01:20:09 +0530

    added pricing-table-seven

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -42,6 +42,7 @@ import { UpcomingChargesDemo } from '@/components/upcoming-charges-demo';
 import { BillingSettings2Demo } from '@/components/billing-settings-2-demo';
 import { PricingTableSixDemo } from '@/components/pricing-table-six-demo';
 import { UsageBasedPricingDemo } from '@/components/usage-based-pricing-demo';
+import { PricingTableSevenDemo } from '@/components/pricing-table-seven-demo';
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
@@ -88,6 +89,7 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
 	  BillingSummaryCardDemo,
     UpcomingChargesDemo,
 	  PricingTableSixDemo,
+    PricingTableSevenDemo,
 	 BillingSettings2Demo,
     UsageBasedPricingDemo
   };
